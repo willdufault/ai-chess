70b4b175ecf716822d77fe3297f16eb3f64dc68f
Will Dufault <wduf02@gmail.com>
2025-08-09 09:47:29 -0400

    move strat test refactor

--- tests/test_move_strategies.py
+++ tests/test_move_strategies.py
@@ -1,7 +1,7 @@
 from unittest import TestCase, main
 
 from enums import Color
-from models.board import Board
+from models.board import DIAGONAL_DIRECTIONS, ORTHOGONAL_DIRECTIONS, Board
 from models.coordinate import Coordinate
 from models.move_strategies import (
     BishopMoveStrategy,
@@ -19,14 +19,14 @@ class TestPawnMoveStrategy(TestCase):
         self.board = Board()
         self.move_strategy = PawnMoveStrategy()
 
-    def test_move_forward_one(self) -> None:
+    def test_move_pawn_forward_one(self) -> None:
         self.assertTrue(
             self.move_strategy.is_valid_move(
                 Color.WHITE, Coordinate(1, 0), Coordinate(2, 0), self.board
             )
         )
 
-    def test_move_forward_one_on_top(self) -> None:
+    def test_move_pawn_forward_one_on_top(self) -> None:
         self.board._set_piece(Coordinate(2, 0), Pawn(Color.WHITE))
         self.board._set_piece(Coordinate(2, 1), Pawn(Color.BLACK))
         self.assertFalse(
@@ -40,7 +40,7 @@ class TestPawnMoveStrategy(TestCase):
             )
         )
 
-    def test_move_forward_two(self) -> None:
+    def test_move_pawn_forward_two(self) -> None:
         self.assertTrue(
             self.move_strategy.is_valid_move(
                 Color.WHITE, Coordinate(1, 0), Coordinate(3, 0), self.board
@@ -56,7 +56,7 @@ class TestPawnMoveStrategy(TestCase):
             )
         )
 
-    def test_move_forward_two_on_top(self) -> None:
+    def test_move_pawn_forward_two_on_top(self) -> None:
         self.board._set_piece(Coordinate(2, 0), Pawn(Color.WHITE))
         self.board._set_piece(Coordinate(2, 1), Pawn(Color.BLACK))
         self.assertFalse(
@@ -70,7 +70,7 @@ class TestPawnMoveStrategy(TestCase):
             )
         )
 
-    def test_move_forward_two_blocked(self) -> None:
+    def test_move_pawn_forward_two_blocked(self) -> None:
         self.board._set_piece(Coordinate(2, 0), Pawn(Color.WHITE))
         self.assertFalse(
             self.move_strategy.is_valid_move(
@@ -78,14 +78,14 @@ class TestPawnMoveStrategy(TestCase):
             )
         )
 
-    def test_move_diagonal_one(self) -> None:
+    def test_move_pawn_diagonal_one(self) -> None:
         self.assertFalse(
             self.move_strategy.is_valid_move(
                 Color.WHITE, Coordinate(1, 0), Coordinate(2, 1), self.board
             )
         )
 
-    def test_capture(self) -> None:
+    def test_pawn_capture(self) -> None:
         self.board._set_piece(Coordinate(2, 0), Pawn(Color.WHITE))
         self.board._set_piece(Coordinate(2, 2), Pawn(Color.BLACK))
         self.assertFalse(
@@ -99,7 +99,7 @@ class TestPawnMoveStrategy(TestCase):
             )
         )
 
-    def test_capture_forward_two(self) -> None:
+    def test_pawn_capture_forward_two(self) -> None:
         self.board._set_piece(Coordinate(3, 0), Pawn(Color.WHITE))
         self.board._set_piece(Coordinate(3, 2), Pawn(Color.BLACK))
         self.assertFalse(
@@ -114,160 +114,203 @@ class TestPawnMoveStrategy(TestCase):
         )
 
     def test_get_row_delta(self) -> None:
-        white = Color.WHITE
-        black = Color.BLACK
-        self.assertEqual(self.move_strategy.get_row_delta(white), black)
-        self.assertEqual(self.move_strategy.get_row_delta(black), black)
+        self.assertEqual(self.move_strategy.get_row_delta(Color.WHITE), 1)
+        self.assertEqual(self.move_strategy.get_row_delta(Color.BLACK), -1)
 
 
 class TestKnightMoveStrategy(TestCase):
     def setUp(self) -> None:
         self.board = Board()
         self.move_strategy = KnightMoveStrategy()
+        self.move_patterns = KnightMoveStrategy.MOVE_PATTERNS
 
-    def test_move_knight(self) -> None:
-        self.assertFalse(
-            self.move_strategy.is_valid_move(
-                Color.WHITE, Coordinate(3, 3), Coordinate(5, 3), self.board
-            )
+    def test_move_knight_valid(self) -> None:
+        for move_pattern in self.move_patterns:
+            row_delta = move_pattern.row_delta
+            col_delta = move_pattern.col_delta
+            with self.subTest(row_delta=row_delta, col_delta=col_delta):
+                self.assertTrue(
+                    self.move_strategy.is_valid_move(
+                        Color.WHITE,
+                        Coordinate(4, 4),
+                        Coordinate(4 + row_delta, 4 + col_delta),
+                    ),
+                    self.board,
+                )
+
+    def test_move_knight_invalid(self) -> None:
+        to_coords = (
+            Coordinate(4, 3),
+            Coordinate(5, 3),
+            Coordinate(4, 4),
+            Coordinate(6, 6),
         )
-        self.assertTrue(
-            self.move_strategy.is_valid_move(
-                Color.WHITE, Coordinate(3, 3), Coordinate(5, 4), self.board
-            )
-        )
-        self.assertTrue(
-            self.move_strategy.is_valid_move(
-                Color.WHITE, Coordinate(3, 3), Coordinate(1, 2), self.board
-            )
-        )
-        self.assertTrue(
-            self.move_strategy.is_valid_move(
-                Color.WHITE, Coordinate(3, 3), Coordinate(5, 2), self.board
-            )
-        )
+        for to_coord in to_coords:
+            with self.subTest(to_coord=to_coord):
+                self.assertFalse(
+                    self.move_strategy.is_valid_move(
+                        Color.WHITE, Coordinate(3, 3), to_coord, self.board
+                    )
+                )
 
 
-class TestKnightMoveStrategy(TestCase):
+class TestBishopMoveStrategy(TestCase):
     def setUp(self) -> None:
         self.board = Board()
         self.move_strategy = BishopMoveStrategy()
+        self.directions = DIAGONAL_DIRECTIONS
 
-    def test_move_bishop(self) -> None:
-        self.assertFalse(
-            self.move_strategy.is_valid_move(
-                Color.WHITE, Coordinate(3, 3), Coordinate(3, 4), self.board
-            )
+    def test_move_bishop_valid(self) -> None:
+        for direction in self.directions:
+            row_delta = direction.row_delta
+            col_delta = direction.col_delta
+            for distance in (1, 2):
+                with self.subTest(
+                    row_delta=row_delta, col_delta=col_delta, distance=distance
+                ):
+                    self.assertTrue(
+                        self.move_strategy.is_valid_move(
+                            Color.WHITE,
+                            Coordinate(4, 4),
+                            Coordinate(
+                                4 + distance * row_delta, 4 + distance * col_delta
+                            ),
+                        ),
+                        self.board,
+                    )
+
+    def test_move_bishop_invalid(self) -> None:
+        to_coords = (
+            Coordinate(3, 4),
+            Coordinate(4, 3),
+            Coordinate(5, 3),
+            Coordinate(5, 4),
         )
-        self.assertTrue(
-            self.move_strategy.is_valid_move(
-                Color.WHITE, Coordinate(3, 3), Coordinate(4, 4), self.board
-            )
-        )
-        self.assertTrue(
-            self.move_strategy.is_valid_move(
-                Color.WHITE, Coordinate(3, 3), Coordinate(1, 1), self.board
-            )
-        )
-        self.assertFalse(
-            self.move_strategy.is_valid_move(
-                Color.WHITE, Coordinate(3, 3), Coordinate(5, 4), self.board
-            )
-        )
+        for to_coord in to_coords:
+            with self.subTest(to_coord=to_coord):
+                self.assertFalse(
+                    self.move_strategy.is_valid_move(
+                        Color.WHITE, Coordinate(3, 3), to_coord, self.board
+                    )
+                )
 
 
 class TestRookMoveStrategy(TestCase):
     def setUp(self) -> None:
         self.board = Board()
         self.move_strategy = RookMoveStrategy()
+        self.directions = ORTHOGONAL_DIRECTIONS
 
-    def test_move_rook(self) -> None:
-        self.assertFalse(
-            self.move_strategy.is_valid_move(
-                Color.WHITE, Coordinate(3, 3), Coordinate(4, 4), self.board
-            )
+    def test_move_rook_valid(self) -> None:
+        for direction in self.directions:
+            row_delta = direction.row_delta
+            col_delta = direction.col_delta
+            for distance in (1, 2):
+                with self.subTest(
+                    row_delta=row_delta, col_delta=col_delta, distance=distance
+                ):
+                    self.assertTrue(
+                        self.move_strategy.is_valid_move(
+                            Color.WHITE,
+                            Coordinate(4, 4),
+                            Coordinate(
+                                4 + distance * row_delta, 4 + distance * col_delta
+                            ),
+                        ),
+                        self.board,
+                    )
+
+    def test_move_rook_invalid(self) -> None:
+        to_coords = (
+            Coordinate(4, 4),
+            Coordinate(4, 5),
+            Coordinate(5, 5),
+            Coordinate(2, 1),
         )
-        self.assertTrue(
-            self.move_strategy.is_valid_move(
-                Color.WHITE, Coordinate(3, 3), Coordinate(4, 3), self.board
-            )
-        )
-        self.assertTrue(
-            self.move_strategy.is_valid_move(
-                Color.WHITE, Coordinate(3, 3), Coordinate(3, 6), self.board
-            )
-        )
-        self.assertFalse(
-            self.move_strategy.is_valid_move(
-                Color.WHITE, Coordinate(3, 3), Coordinate(2, 1), self.board
-            )
-        )
+        for to_coord in to_coords:
+            with self.subTest(to_coord=to_coord):
+                self.assertFalse(
+                    self.move_strategy.is_valid_move(
+                        Color.WHITE, Coordinate(3, 3), to_coord, self.board
+                    )
+                )
 
 
 class TestQueenMoveStrategy(TestCase):
     def setUp(self) -> None:
         self.board = Board()
         self.move_strategy = QueenMoveStrategy()
+        self.directions = ORTHOGONAL_DIRECTIONS + DIAGONAL_DIRECTIONS
 
-    def test_move_queen(self) -> None:
-        self.assertTrue(
-            self.move_strategy.is_valid_move(
-                Color.WHITE, Coordinate(3, 3), Coordinate(4, 4), self.board
-            )
+    def test_move_queen_valid(self) -> None:
+        for direction in self.directions:
+            row_delta = direction.row_delta
+            col_delta = direction.col_delta
+            for distance in (1, 2):
+                with self.subTest(
+                    row_delta=row_delta, col_delta=col_delta, distance=distance
+                ):
+                    self.assertTrue(
+                        self.move_strategy.is_valid_move(
+                            Color.WHITE,
+                            Coordinate(4, 4),
+                            Coordinate(
+                                4 + distance * row_delta, 4 + distance * col_delta
+                            ),
+                        ),
+                        self.board,
+                    )
+
+    def test_move_queen_invalid(self) -> None:
+        to_coords = (
+            Coordinate(4, 5),
+            Coordinate(5, 4),
+            Coordinate(5, 6),
+            Coordinate(4, 1),
         )
-        self.assertTrue(
-            self.move_strategy.is_valid_move(
-                Color.WHITE, Coordinate(3, 3), Coordinate(4, 3), self.board
-            )
-        )
-        self.assertTrue(
-            self.move_strategy.is_valid_move(
-                Color.WHITE, Coordinate(3, 3), Coordinate(5, 5), self.board
-            )
-        )
-        self.assertTrue(
-            self.move_strategy.is_valid_move(
-                Color.WHITE, Coordinate(3, 3), Coordinate(2, 4), self.board
-            )
-        )
-        self.assertFalse(
-            self.move_strategy.is_valid_move(
-                Color.WHITE, Coordinate(3, 3), Coordinate(5, 7), self.board
-            )
-        )
+        for to_coord in to_coords:
+            with self.subTest(to_coord=to_coord):
+                self.assertFalse(
+                    self.move_strategy.is_valid_move(
+                        Color.WHITE, Coordinate(3, 3), to_coord, self.board
+                    )
+                )
 
 
 class TestKingMoveStrategy(TestCase):
     def setUp(self) -> None:
         self.board = Board()
         self.move_strategy = KingMoveStrategy()
+        self.move_patterns = KingMoveStrategy.MOVE_PATTERNS
 
-    def test_move_king(self) -> None:
-        self.assertTrue(
-            self.move_strategy.is_valid_move(
-                Color.WHITE, Coordinate(3, 3), Coordinate(4, 4), self.board
-            )
+    def test_move_king_valid(self) -> None:
+        for move_pattern in self.move_patterns:
+            row_delta = move_pattern.row_delta
+            col_delta = move_pattern.col_delta
+            with self.subTest(row_delta=row_delta, col_delta=col_delta):
+                self.assertTrue(
+                    self.move_strategy.is_valid_move(
+                        Color.WHITE,
+                        Coordinate(4, 4),
+                        Coordinate(4 + row_delta, 4 + col_delta),
+                    ),
+                    self.board,
+                )
+
+    def test_move_king_invalid(self) -> None:
+        to_coords = (
+            Coordinate(4, 5),
+            Coordinate(5, 4),
+            Coordinate(5, 5),
+            Coordinate(5, 3),
         )
-        self.assertTrue(
-            self.move_strategy.is_valid_move(
-                Color.WHITE, Coordinate(3, 3), Coordinate(4, 3), self.board
-            )
-        )
-        self.assertTrue(
-            self.move_strategy.is_valid_move(
-                Color.WHITE, Coordinate(3, 3), Coordinate(2, 3), self.board
-            )
-        )
-        self.assertTrue(
-            self.move_strategy.is_valid_move(
-                Color.WHITE, Coordinate(3, 3), Coordinate(4, 2), self.board
-            )
-        )
-        self.assertFalse(
-            self.move_strategy.is_valid_move(
-                Color.WHITE, Coordinate(3, 3), Coordinate(5, 3), self.board
-            )
-        )
+        for to_coord in to_coords:
+            with self.subTest(to_coord=to_coord):
+                self.assertFalse(
+                    self.move_strategy.is_valid_move(
+                        Color.WHITE, Coordinate(3, 3), to_coord, self.board
+                    )
+                )
 
 
 if __name__ == "__main__":
